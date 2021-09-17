75dab28b171edc1570a0b16c717f4cb2e65aa762
brian-anderson01 <54916029+brian-anderson01@users.noreply.github.com>
2021-09-17 17:50:24 -0400

    added dns powershell script

---
+++ code/week3/dns-resolver.ps1
@@ -0,0 +1,8 @@
+﻿param ($networkpre, $serverip)
+
+for ($i=1; $i -le 255; $i++) {
+
+    Resolve-DnsName -DnsOnly "$networkpre.$i" -Server $serverip -ErrorAction Ignore |
+    Select-Object Name, NameHost
+
+}
